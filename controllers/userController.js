f00e9f76c53a22e936a29e9ae1d789ac5a95cbda
Elham <elhamwaheed777@gmail.com>
2025-04-24 00:02:05 +0500

    Working in userRouter

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+// Route for User Login
+const loginUser = async (req,res) => {
+
+}
+
+// Route for user Register
+const registerUser = async (req,res)=>{
+
+} 
+
+// Route for Admin Login
+const adminLogin = async (req,res)=>{
+
+} 
+
+
+export {loginUser,registerUser,adminLogin}
