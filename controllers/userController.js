9912ef2b4c34bcea252495087de9a13cf5ad7ed2
Elham <elhamwaheed777@gmail.com>
2025-04-24 01:04:05 +0500

    Working in User Registration for an Account

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,47 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id },);
+};
+
 // Route for User Login
 const loginUser = async (req, res) => {};
 
 // Route for user Register
 const registerUser = async (req, res) => {
-  res.json({ msg: "Register API Working" });
+  try {
+    const { name, email, password } = req.body;
+
+    // checking user already exists or not
+    const exists = await userModel.findOne({ email });
+
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    // validator email format & strong password
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Please enter a valid email" });
+    }
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Please enter a strong password" });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+  } catch (e) {}
 };
 
 // Route for Admin Login
