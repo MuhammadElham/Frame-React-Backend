7d2dab663e7f12ab38c70c68a2b4b8e195a1f943
Elham <elhamwaheed777@gmail.com>
2025-04-24 00:32:05 +0500

    Successfull in Routing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,12 @@
-
-
-
-
-
 // Route for User Login
-const loginUser = async (req,res) => {
-
-}
+const loginUser = async (req, res) => {};
 
 // Route for user Register
-const registerUser = async (req,res)=>{
-
-} 
+const registerUser = async (req, res) => {
+  res.json({ msg: "Register API Working" });
+};
 
 // Route for Admin Login
-const adminLogin = async (req,res)=>{
+const adminLogin = async (req, res) => {};
 
-} 
-
-
-export {loginUser,registerUser,adminLogin}
+export { loginUser, registerUser, adminLogin };
