4c1286862fdeaf481788afee5c8170a748798574
Elham <elhamwaheed777@gmail.com>
2025-04-24 23:18:24 +0500

    Login Working Done

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,30 @@ const createToken = (id) => {
 };
 
 // Route for User Login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Matching for Email
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exists" });
+    }
+
+    // Matching for Password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      // generate the token
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Route for user Register
 const registerUser = async (req, res) => {
@@ -44,8 +67,7 @@ const registerUser = async (req, res) => {
 
     const token = createToken(user._id);
 
-    res.json({ success: true, token});
-
+    res.json({ success: true, token });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
