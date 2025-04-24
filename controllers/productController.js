8a2bd6592ebb5a7ed322aaa970ca797ad49387a7
Elham <elhamwaheed777@gmail.com>
2025-04-24 23:46:57 +0500

    productController Working and Routes Done for ProductRoute

---
+++ controllers/productController.js
@@ -0,0 +1,22 @@
+
+// function for Add product
+const addProduct = async (req, res) => {
+
+};
+
+// function for List product
+const listProduct = async (req, res) => {
+
+};
+
+// function for Remove product
+const removeProduct = async (req, res) => {
+
+};
+
+// function for Single product Info
+const singleProduct = async (req, res) => {
+
+};
+
+export { addProduct, listProduct, removeProduct, singleProduct };
