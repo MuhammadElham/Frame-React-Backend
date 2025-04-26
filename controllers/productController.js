5d98f43889c52efe303e067d3cc804560e3d48ce
Elham <elhamwaheed777@gmail.com>
2025-04-26 22:12:46 +0500

    Error File Upload resolve nahi howa he

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,50 +1,24 @@
 // function for Add product
-// const addProduct = async (req, res) => {
-//   try {
-
-//     console.log("Files Received: " , req.files)
-
-//     const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
-
-//     // Issue
-//     const image1 = req.files.image1 && req.files.image1[0];
-//     const image2 = req.files.image2 && req.files.image2[0];
-//     const image3 = req.files.image3 && req.files.image3[0];
-//     const image4 = req.files.image4 && req.files.image4[0];
-
-//     console.log(name, "\n", description, "\n", price, "\n", category, "\n", subCategory, "\n", sizes, "\n", bestseller);
-//     console.log(image1, image2, image3, image4);
-
-//     res.json({});
-//   } catch (error) {
-//     console.log(error);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
 const addProduct = async (req, res) => {
   try {
-    console.log("Files Received: ", req.files);
-    console.log("Body Received: ", req.body);
-
+    // console.log("Files Received: " , req.files)
     const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
 
-    const imageFiles = [];
-    if (req.files.image1) imageFiles.push(req.files.image1[0].path);
-    if (req.files.image2) imageFiles.push(req.files.image2[0].path);
-    if (req.files.image3) imageFiles.push(req.files.image3[0].path);
-    if (req.files.image4) imageFiles.push(req.files.image4[0].path);
+    // Issue
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
 
-    console.log("Images Path:", imageFiles);
+    // console.log(name, "\n", description, "\n", price, "\n", category, "\n", subCategory, "\n", sizes, "\n", bestseller);
+    // console.log(image1, image2, image3, image4);
 
-    res.json({ success: true, message: "Product Added Successfully" });
+    res.json({});
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
-
-
-
 
 // function for List product
 const listProduct = async (req, res) => {};
--- middleware/multer.js
+++ middleware/multer.js
@@ -5,12 +5,10 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/'); // Make sure uploads folder exists!
   },
   filename: function (req, file, cb) {
-    console.log("Uploaded file:", file.originalname);
     cb(null, file.originalname);
   }
 });
 
 const upload = multer({ storage });
-console.log("Uploads = ", upload);
 
 export default upload;
