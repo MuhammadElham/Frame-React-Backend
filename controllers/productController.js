779a9287fc01439c2a7b53f333b4e670724036df
Elham <elhamwaheed777@gmail.com>
2025-04-27 04:34:44 +0500

    Removed Product Done Successfully

--- controllers/productController.js
+++ controllers/productController.js
@@ -59,7 +59,15 @@ const listProduct = async (req, res) => {
 };
 
 // function for Remove product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Product Removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for Single product Info
 const singleProduct = async (req, res) => {};
