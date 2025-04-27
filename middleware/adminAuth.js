937415139e4084fac83b62e459591400e475de2f
Elham <elhamwaheed777@gmail.com>
2025-04-28 00:07:52 +0500

    Working in adminAuth Done

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -6,7 +6,16 @@ const adminAuth = async (req, res, next) => {
     const { token } = req.headers;
 
     if (!token) {
-      res.json({ success: false, message: "Not Authorized Login Again!" });
+      return res.json({ success: false, message: "Not Authorized Login Again!" });
     }
-  } catch (error) {}
+    const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+    if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+      return res.json({ success: false, message: "Not Authorized Login Again!" });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
+export default adminAuth;
--- middleware/multer.js
+++ middleware/multer.js
@@ -2,22 +2,10 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   filename: function (req, file, callback) {
-    console.log("Uploaded file = ", file.originalname);
     callback(null, file.originalname);
   },
 });
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, callback) {
-//     callback(null, 'uploads/'); // <-- Folder batana zaruri!
-//   },
-//   filename: function (req, file, callback) {
-//     callback(null, Date.now() + '-' + file.originalname);
-//   }
-// });
-
-
 const upload = multer({ storage });
-console.log("Uploads = ", upload);
 
 export default upload;
