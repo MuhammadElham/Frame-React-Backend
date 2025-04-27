7ae7c6fa1d83528def69bd908db63642f63c55e5
Elham <elhamwaheed777@gmail.com>
2025-04-27 23:43:15 +0500

    Working in adminLogin

--- controllers/userController.js
+++ controllers/userController.js
@@ -75,6 +75,14 @@ const registerUser = async (req, res) => {
 };
 
 // Route for Admin Login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+    }
+  } catch (error) {}
+};
 
 export { loginUser, registerUser, adminLogin };
