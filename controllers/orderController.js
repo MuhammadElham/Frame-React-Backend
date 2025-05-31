e52d6ec044d0fb1f57b4cbab96cdab7c356374ea
Elham <elhamwaheed777@gmail.com>
2025-05-31 14:58:54 +0500

    Working in Payment Method

---
+++ controllers/orderController.js
@@ -0,0 +1,35 @@
+// Placing order using COD Method
+const placeOrder = async (req, res) => {
+
+};
+// Placing order using Stripe Method
+const placeOrderStripe = async (req, res) => {
+
+};
+
+// Placing order using RazorPay Method
+const placeOrderRazorpay = async (req, res) => {
+
+};
+
+// All Orders data for Admin Panel
+const allOrders = async (req, res) => {
+
+};
+
+// User Order Data from Frontend
+const userOrders = async (req, res) => {
+    
+};
+
+// Update Order Status from Admin Panel
+const updateStatus = async (req, res) => {
+
+};
+
+// Placing order using JazzCash Method
+const placeOrderJazzcash = async (req, res) => {
+
+};
+
+export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, placeOrderJazzcash}
